8fcf1a368ce9a1614c399601b13a0c40fc6f5ce9
Josué Eduardo García <josueduardo.garcia@gmail.com>
2024-07-24 08:23:07 -0600

    Arreglando el ejercicio 1

--- GE240098_DM240115DesPra01_DAW/Ejercicio01/js/jubilarse.js
+++ GE240098_DM240115DesPra01_DAW/Ejercicio01/js/jubilarse.js
@@ -4,15 +4,19 @@ let edad = parseInt(prompt('Digite su edad: '))
 let sexo = prompt('Digite su sexo (M/F): ')
 
 if(edad >= 60 && sexo === 'M'){
-  document.write(`<h1>${name}, sí se puede jubilar</h1>`)
+  document.write(`<h1>${name}, sí se puede jubilar</h1>
+                  <img src="img/dedoarriba.jpg"/>`)
 }
 else if (edad < 60 && sexo === 'M'){
-  document.write(`<h1>${name}, no puede jubilarse aún le faltan ${60 - edad} años`)
+  document.write(`<h1>${name}, no puede jubilarse aún le faltan ${60 - edad} años
+                  <img src="img/dedoabajo.avif"/>`)
 }
 
 if(edad >= 54 && sexo === 'F'){
-  document.write(`<h1>${name}, sí se puede jubilar</h1>`)
+  document.write(`<h1>${name}, sí se puede jubilar</h1>
+                  <img src="img/dedoarriba.jpg"/>`)
 }
 else if (edad < 60 && sexo === 'F'){
-  document.write(`<h1>${name}, no puede jubilarse aún le faltan ${54 - edad} años`)
+  document.write(`<h1>${name}, no puede jubilarse aún le faltan ${54 - edad} años
+                  <img src="img/dedoabajo.avif"/>`)
 }

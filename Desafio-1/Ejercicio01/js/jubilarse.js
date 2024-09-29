857865cb1438dca31b55ed0c5c3f0a80f08d3b60
Josué Eduardo García <josueduardo.garcia@gmail.com>
2024-09-28 20:52:16 -0600

    Desafío #1

---
+++ Desafio-1/Ejercicio01/js/jubilarse.js
@@ -0,0 +1,22 @@
+alert('Taller Practico 01 - DAW')
+let name = prompt('Digite su nombre: ')
+let edad = parseInt(prompt('Digite su edad: '))
+let sexo = prompt('Digite su sexo (M/F): ')
+
+if(edad >= 60 && sexo === 'M'){
+  document.write(`<h1>${name}, sí se puede jubilar</h1>
+                  <img src="img/dedoarriba.jpg"/>`)
+}
+else if (edad < 60 && sexo === 'M'){
+  document.write(`<h1>${name}, no puede jubilarse aún le faltan ${60 - edad} años
+                  <img src="img/dedoabajo.avif"/>`)
+}
+
+if(edad >= 54 && sexo === 'F'){
+  document.write(`<h1>${name}, sí se puede jubilar</h1>
+                  <img src="img/dedoarriba.jpg"/>`)
+}
+else if (edad < 60 && sexo === 'F'){
+  document.write(`<h1>${name}, no puede jubilarse aún le faltan ${54 - edad} años
+                  <img src="img/dedoabajo.avif"/>`)
+}
